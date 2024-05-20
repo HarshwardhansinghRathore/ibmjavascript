74cd0d265d79838f77e394212ee8fab3949fae4c
HarshwardhansinghRathore <hrathore076@gmail.com>
2024-05-20 09:58:58 -0400

    added word analysis

---
+++ speedAnalysis/speed_analysis.js
@@ -0,0 +1,38 @@
+let testText = 'the quick brown for jumps over he laz dog'
+let startTime, endTime
+
+function startTest() {
+    document.getElementById('inputText').value = testText;
+    document.getElementById('output').innerHTML = ''
+    startTime = new Date().getTime();
+
+
+    var button = document.getElementById('btn');
+    button.innerHTML = 'End test';
+    button.onclick = endTest;
+}
+
+function endTest(){
+    endTime = new Date().getTime();
+    document.getElementById('userInput').readOnly = true;
+
+    var timeElapsed  = (endTime - startTime)/1000;
+    var userTypedText = document.getElementById('userInput').value;
+    
+    var typedWords = userTypedText.split(/\s+/).filter(function (word){
+        return word !== '';
+    }).length;
+    var wpn = 0;
+    if (timeElapsed !== 0 && !isNaN(typedWords)) {
+        wpm= Math.round((typedWords/timeElapsed)*60);
+    }
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '<h2>ttyping test Results:</h2>'+
+    '<p>Words Typed:'+typedWords+'</p>'+
+    "<p>"+timeElapsed.toFixed(2)+" seconds</p>"+
+    "<p> Words Per Minute (WPM): "+wpm+'</p>';
+
+    var button = document.getElementById('btn');
+    button.innerHTML = 'Start Test';
+    button.onclick = startTest;
+}
